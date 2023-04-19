9fd4d122528f2812041f971391a3e915e5a7d4d3
Olena <7890572@ukr.net>
2023-04-19 12:32:02 +0200

    index.js

--- index.js
+++ index.js
@@ -1,41 +1,37 @@
 import { getUserByName, getUserInfractions } from './user-api.js';
 
-const getReasonForWorstInfractionLinkified = (username, callback) => {
-    getUserByName(username, (user) => {
-        getUserInfractions(user.id, (result) => {
-            let foundIndex = 0;
-            for(let i = 1; i < result.length; i++) {
-                if(result[i].id > result[foundIndex].id) {
-                    foundIndex = i;
-                };
-            };
-            const { reason } = result[foundIndex];
-            callback(reason.replace(/\b(http)(s)?:\/\/\S+/, match => `<a href='${match}' > ${match}</a>`)) 
-        });
-    });
-};
-
-const getReasonForMostRecentInfractionLinkified = (username, callback) => {
-    getUserByName(username, (user) => {
-	    getUserInfractions(user.id, (result) => {
-	        let foundPoints = 0;
-	        for (let i = 1; i < result.length; i++)	{
-		        if (result[i].points > result[foundPoints].points) {
-		            foundPoints = i;
-		        };
-	        };
-            const { reason } = result[foundPoints];
-	        callback(reason.replace(/\b(http)(s)?:\/\/\S+/, match => `<a href='${match}' > ${match}</a>`));
-	    });
-    });
-};
-
+const urlReason = (reason) => {
+  return reason.replace(/\b(http)(s)?:\/\/\S+/, match => `<a href='${match}' > ${match}</a>`);
+}
+  
+const getReasonForWorstInfractionLinkified = async (username) => {
+  try {
+    const user = await getUserByName(username);
+    const result = await getUserInfractions(user.id);
+    let foundIndex = 0;
+    let foundPoints = 0;
+    for(let i = 1; i < result.length; i++) {
+      if(result[i].id > result[foundIndex].id) {
+          foundIndex = i;
+      }
+      if(result[i].points > result[foundPoints].points) {
+        foundPoints = i;
+      }
+    };
+    return {
+      worst: urlReason(result[foundPoints].reason),
+      mostRecent: urlReason(result[foundIndex].reason)
+    }
+    } catch (error) {
+      console.log(`Error: ${error}`);
+      return { worst: "", mostRecent: "" };
+    }
+}
+  
 export const getRelevantInfractionReasons = (username) => {
-    return new Promise((resolve) =>	{
-	    getReasonForWorstInfractionLinkified(username, (worst) => {
-	        getReasonForMostRecentInfractionLinkified(username, (mostRecent) => {
-		        resolve({mostRecent, worst});
-		    });
-	    });
-    });
+  return new Promise((resolve, reject) => {
+    getReasonForWorstInfractionLinkified(username)
+      .then(({ worst, mostRecent }) => resolve({ worst, mostRecent }))
+      .catch(error => reject(error));
+  });
 };
